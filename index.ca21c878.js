c129562e8c9979fc48b78b564f61f0867ca7bc51
haopro206 <haogaming52@gmail.com>
2026-04-05 13:02:09 +0700

    fix3

--- index.ca21c878.js
+++ index.ca21c878.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const data = {
     name: "Nguyễn Ngọc Hảo",
-    avatar: "avatar.jpg",  // ← ĐỔI LẠI ĐƯỜNG DẪN ĐÚNG CỦA BẠN
-    desc: "2011 , đếch biết sợ và thjch bị ăn đòn",
+    avatar: "avatar.jpg",
+    desc: "2011, đếch biết sợ và thích bị ăn đòn",
     social: [
       { icon: "fa-facebook-f", link: "https://www.facebook.com/haovjppro206/" },
       { icon: "fa-instagram", link: "https://www.instagram.com/_.ngnghao._/" },
-    
+      { icon: "fa-comment-dots", link: "https://ngl.link/_.ngnghao._2", },  // Có icon + chữ ngl
       { icon: "fa-tiktok", link: "https://www.tiktok.com/@nnhchechou" }
     ]
   };
@@ -23,16 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
             <div class="image"></div>
           </div>
           <div class="details">
-            <div class="name-box">
-              <div class="avatar-small"></div>
-              <h3 class="name">${data.name}</h3>
-            </div>
+            <h3 class="name">${data.name}</h3>
             <p class="excerpt">${data.desc}</p>
             <ul class="social">
               ${data.social.map(s => `
                 <li>
                   <a href="${s.link}" target="_blank">
-                    <i class="fab ${s.icon}"></i>
+                    <i class="${s.icon === 'fa-comment-dots' ? 'fas' : 'fab'} ${s.icon}"></i>
+                    ${s.name ? `<span style="font-size: 11px; margin-left: 5px;">${s.name}</span>` : ''}
                   </a>
                 </li>
               `).join("")}
@@ -43,23 +41,12 @@ document.addEventListener("DOMContentLoaded", function () {
     </div>
   `;
 
-  // Set avatar cho ảnh lớn
+  // Set avatar
   const avatarImage = document.querySelector(".avatar .image");
   if (avatarImage) {
     avatarImage.style.backgroundImage = `url('${data.avatar}')`;
     avatarImage.style.backgroundSize = "cover";
     avatarImage.style.backgroundPosition = "center";
     avatarImage.style.backgroundRepeat = "no-repeat";
-    console.log("Đã set avatar:", data.avatar); // Kiểm tra log
-  } else {
-    console.log("Không tìm thấy .avatar .image");
-  }
-
-  // Set avatar cho ảnh nhỏ
-  const smallAvatar = document.querySelector(".avatar-small");
-  if (smallAvatar) {
-    smallAvatar.style.backgroundImage = `url('${data.avatar}')`;
-    smallAvatar.style.backgroundSize = "cover";
-    smallAvatar.style.backgroundPosition = "center";
   }
 });
