45c713f6daf63d8a149f16b2f66b5751e3c78fb1
haopro206 <haogaming52@gmail.com>
2026-09-12 11:39:34 +0700

    new

--- blockf12.js
+++ blockf12.js
@@ -1,21 +1,46 @@
-var listchan = ['&', 'charCodeAt', 'firstChild', 'href', 'join', 'match', '+', '=', 'TK', '<a href=\'/\'>x</a>', 'innerHTML', 'fromCharCode', 'split', 'constructor', 'a', 'div', 'charAt', '', 'toString', 'createElement', 'debugger', '+-a^+6', 'Fingerprint2', 'KT', 'TKK', 'substr', '+-3^+b+-f', '67bc0a0e207df93c810886524577351547e7e0459830003d0b8affc987d15fd7', 'length', 'get', '((function(){var a=1585090455;var b=-1578940101;return 431433+"."+(a+b)})())', '.', 'https?:\/\/', ''];
 (function () {
-    console.log("%c F12 CÁI CON CẶC! %c", 'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;', "font-size:12px;color:#999999;");
+  'use strict';
 
-    (function block_f12() {
+  // 1. Chặn chuột phải & các phím tắt
+  document.addEventListener('contextmenu', e => e.preventDefault());
+  document.addEventListener('keydown', e => {
+    if (
+      e.key === 'F12' || e.keyCode === 123 ||
+      (e.ctrlKey && e.shiftKey && ['I','i','J','j','C','c'].includes(e.key)) ||
+      (e.ctrlKey && ['u','U','s','S'].includes(e.key))
+    ) {
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    }
+  });
+
+  // 2. Thông báo trong Console
+  console.log(
+    "%c F12 CÁI CON CẶC! %c", 
+    'font-family: sans-serif; font-size: 22px; color: #ff2a7a; font-weight: bold;', 
+    "font-size: 12px; color: #888;"
+  );
+
+  // 3. Treo tab DevTools bằng vòng lặp Debugger (có ngắt nhịp để máy không bị lag)
+  function freezeDevTools() {
+    (function () {
+      (function a() {
         try {
-            (function chanf12(dataf) {
-                if ((listchan[33] + (dataf / dataf))[listchan[28]] !== 1 || dataf % 20 === 0) {
-
-                    (function () { })[listchan[13]](listchan[20])()
-                } else {
-                    debugger;
-
-                };
-                chanf12(++dataf)
-            }(0))
+          (function b(i) {
+            if (('' + (i / i)).length !== 1 || i % 20 === 0) {
+              (function () {}).constructor('debugger')();
+            } else {
+              debugger;
+            }
+            b(++i);
+          })(0);
         } catch (e) {
-            setTimeout(block_f12, 5000)
+          setTimeout(a, 1000);
         }
-    })()
+      })();
+    })();
+  }
+
+  freezeDevTools();
 })();
